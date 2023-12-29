8ac2175363a5af68908a1cdc4d986e789df391ef
Veronika <veronikapetrushka@gmail.com>
2023-12-29 13:13:40 +0100

    js done, issue with console, no css yet

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login - form");
+loginForm.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (login === "" || password === "") {
+    return console.log("All form fields must be filled in");
+  }
+
+  const emailValue = trim(email.value);
+  const passwordValue = trim(password.value);
+
+  console.log(`Email: ${emailValue}, Password: ${passwordValue}`);
+  form.reset();
+}
--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector(".boxes");
+
+function createBoxes(amount) {
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+
+    boxesContainer.append(box);
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(input.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  }
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
